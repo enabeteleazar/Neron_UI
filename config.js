bcd88c09efa2562ee642ed154a7baa66aaa63a9a
enabeteleazar <nabet.eleazar@gmail.com>
2026-03-15 07:59:43 +0000

    Ignore config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-// client/config.js
-// ⚠️  NE PAS COMMITER CE FICHIER — il contient des secrets
-
-window.NERON_CONFIG = {
-  API_URL: 'http://100.121.73.19:8000',
-  API_KEY: 'tzJewhgys-4v8KlRcCMyAyI12VBcPdwXHlT03tHCVC8',
-};
