8002fd74b533210695fd3de48b7219fb664919ed
enabeteleazar <nabet.eleazar@gmail.com>
2026-04-05 17:18:32 +0200

    netoyage du projet

--- neron-client/app/page.tsx
+++
@@ -1,5 +0,0 @@
-import PhoneShell from "@/components/PhoneShell";
-
-export default function Home() {
-  return <PhoneShell />;
-}
--- neron-client/next.config.ts
+++
@@ -1,7 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  // App Router activé par défaut en Next.js 15
-};
-
-export default nextConfig;
